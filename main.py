52ffdd97fcddbd010fe5e5de2f2bb35c68776072
kaapyth0n <nikita.kapitonov@gmail.com>
2023-08-16 19:54:11 +0200

    1

--- main.py
+++ main.py
@@ -53,4 +53,66 @@ class PlayerEcsample(BoxLayout):
         self.file_name.text = self.sound.source
 
     def play_music(self):
-        pass
+        self.play.disabled = True
+        self.pausa.disabled = False
+        self.stop.disabled = False
+        self.sound.play()
+        self.timer.start()
+
+    def pause_music(self):
+        self.timer.cancel()
+        pos = self.sound.get_pos()
+        self.sound.stop()
+        self.slider.value = pos
+        self.play.disabled = False
+        self.pausa.disabled = True
+        self.timer = Timer(1, self.position)
+
+    def stop_music(self):
+        self.sound.stop()
+        self.timer.cancel()
+        self.play.disabled = False
+        self.timer = Timer(1, self.position)
+        self.slider.value = 0
+        self.sound.seek(0)
+        self.stop.disabled = True
+        self.pausa.disabled = True
+
+    def position(self):
+        self.timer = Timer(1, self.position)
+        self.slider.value = self.sound.get_pos()
+        self.timer.start()
+        self.seconds += 1
+        self.time_format(self.seconds)
+        if self.slider.value == 0:
+            self.stop_music()
+            self.slider.value = 0
+            self.seconds = 0
+            self.time_format(self.seconds)
+
+    def music_position(self, instance):
+        if self.sound != None:
+            self.sound.seek(instance.value)
+            self.seconds = instance.value
+            self.time_format(self.seconds)
+
+    def time_format(self, seconds):
+        m, s = divmod(seconds, 60)
+        t = "%02d:%02d" % (m, s)
+        self.time.text = t
+
+
+class PlayerApp(App):
+    player = None
+    def build(self):
+        Window.bind(on_close = self.on_request_close)
+        self.player = PlayerEcsample()
+        return self.player
+
+    def on_request_close(self, *args):
+        self.player.sound.stop()
+        self.player.timer.cancel()
+
+
+if __name__ == "__main__":
+    PlayerApp().run()
